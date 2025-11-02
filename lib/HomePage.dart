c090e218cba27cefc516b43427464731765183ce
Kawsar <alkawsar1030@gmail.com>
2025-11-02 15:59:25 +0600

    taking input from textField then add the value in list and showing the vlues in listview

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -9,20 +9,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController controller = TextEditingController();
-  final TextEditingController controller1 = TextEditingController();
-  double num1 = 0;
-  double num2 = 0;
-  double result = 0;
 
-  void clearText() {
-    controller.clear(); // এই লাইনটি TextField-এর লেখা মুছে দেবে
-  }
-
-  bool showResult = false; // প্রথমে টেক্সটটা লুকানো থাকবে
-
-  void addNumbers() {
+  String item = "";
+  List itemList=[];
+  setItem(value){
     setState(() {
-      showResult = true; // বাটনে চাপলে টেক্সট দেখাবে
+     item=value;
+    });
+  }
+  addItem(value){
+    setState(() {
+      itemList.add(item);
     });
   }
 
@@ -30,50 +27,46 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My counterApp')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: controller,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextFormField(
+            //controller: controller,
+            onChanged: (value) {
+              setItem(value);
 
-              onChanged: (value) {
-                setState(() {
-                  num1 = double.parse(value);
-                });
-              },
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: controller1,
-              onChanged: (value) {
-                setState(() {
-                  num2 = double.parse(value);
-                });
-              },
-            ),
-            SizedBox(height: 10),
 
-            ElevatedButton(
-              onPressed: () {
-                addNumbers();
-                setState(() {
-                 // showResult = true;
-                  result = num1 + num2;
-                  controller1.clear();
-                  controller.clear();
-                });
-              },
-              child: Text('Add these'),
 
-            ),
-            SizedBox(height: 10),
+            },
+          ),
+          SizedBox(height: 5),
+          ElevatedButton(
+            onPressed: () {
 
-            if (showResult)
-              Text('The Result is $result', style: TextStyle(fontSize: 16)),
-          ],
-        ),
+              addItem(item);
+
+            },
+            child: Text('Add these'),
+          ),
+          SizedBox(height: 5),
+
+         SingleChildScrollView(
+           child: Container(
+             height: 400,
+             child: ListView.builder(
+               shrinkWrap: true,
+               itemCount: itemList.length,
+                 itemBuilder: (context, index) {
+                 return ListTile(
+                   title: Text("(${index+1}) ${itemList[index]}"),
+                 );
+             
+                 },
+             ),
+           ),
+         ),
+        ],
       ),
     );
   }
