2d90f0a58fa84776d9543e48135bcb0a2233047f
Kawsar <alkawsar1030@gmail.com>
2025-11-02 11:12:43 +0600

    Showong Text and result after clicking Botton

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -9,66 +9,72 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController controller = TextEditingController();
-  double num1=0;
-  double num2=0;
-  double result=0;
+  final TextEditingController controller1 = TextEditingController();
+  double num1 = 0;
+  double num2 = 0;
+  double result = 0;
+
   void clearText() {
     controller.clear(); // এই লাইনটি TextField-এর লেখা মুছে দেবে
   }
-  // void counter() {
-  //   setState(() {
-  //    num1+num2;
-  //   });
-  // }
+
+  bool showResult = false; // প্রথমে টেক্সটটা লুকানো থাকবে
+
+  void addNumbers() {
+    setState(() {
+      showResult = true; // বাটনে চাপলে টেক্সট দেখাবে
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My counterApp'),),
+      appBar: AppBar(title: Text('My counterApp')),
       body: Center(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-           TextFormField(
-
-              onChanged: (value) {
-                setState(() {
-                  num1=double.parse(value);
-                });
-              },
-           ) ,
-            SizedBox(height: 10,),
             TextFormField(
+              controller: controller,
+
               onChanged: (value) {
                 setState(() {
-                  num2=double.parse(value);
-
-
+                  num1 = double.parse(value);
                 });
               },
-            ) ,
-           SizedBox(height: 10,),
-           ElevatedButton(onPressed: (){
-             setState(() {
-               result= num1+num2;
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: controller1,
+              onChanged: (value) {
+                setState(() {
+                  num2 = double.parse(value);
+                });
+              },
+            ),
+            SizedBox(height: 10),
 
-             });
-             clearText();
+            ElevatedButton(
+              onPressed: () {
+                addNumbers();
+                setState(() {
+                 // showResult = true;
+                  result = num1 + num2;
+                  controller1.clear();
+                  controller.clear();
+                });
+              },
+              child: Text('Add these'),
 
-           },
-               child: Text('Add these')
-           ),
-            SizedBox(height: 10,),
-            Text('The Resust is ${result}'),
+            ),
+            SizedBox(height: 10),
 
-
+            if (showResult)
+              Text('The Result is $result', style: TextStyle(fontSize: 16)),
           ],
         ),
       ),
-
     );
   }
-
- 
 }
